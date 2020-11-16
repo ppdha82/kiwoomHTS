9cf018f94e24f3cccf61c1c2ca6f5673bf40c706
ppdha82@gmail.com <ppdha82@gmail.com>
2020-11-17 01:22:44 +0900

    [Add] Apply Login function and UI

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -39,8 +39,21 @@
             this.nameLabel = new System.Windows.Forms.Label();
             this.idLabel = new System.Windows.Forms.Label();
             this.serverLabel = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.balanceCheckButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.totalPurchaseLabel = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.totalEstimateLabel = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.totalProfitLabel = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.totalProfitRateLabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI1)).BeginInit();
             this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // axKHOpenAPI1
@@ -57,24 +70,27 @@
             this.tableLayoutPanel1.BackColor = System.Drawing.Color.White;
             this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
             this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
-            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 4);
             this.tableLayoutPanel1.Controls.Add(this.accountComboBox, 1, 0);
-            this.tableLayoutPanel1.Controls.Add(this.nameLabel, 1, 1);
-            this.tableLayoutPanel1.Controls.Add(this.idLabel, 1, 2);
-            this.tableLayoutPanel1.Controls.Add(this.serverLabel, 1, 3);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(132, 59);
+            this.tableLayoutPanel1.Controls.Add(this.nameLabel, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.idLabel, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.serverLabel, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.passwordTextBox, 1, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(30, 24);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 4;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 46F));
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 29F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 48.45361F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 51.54639F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 46F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(312, 204);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 27F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(231, 150);
             this.tableLayoutPanel1.TabIndex = 1;
             // 
             // label1
@@ -89,7 +105,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(4, 48);
+            this.label2.Location = new System.Drawing.Point(4, 56);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(65, 12);
             this.label2.TabIndex = 1;
@@ -98,7 +114,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(4, 100);
+            this.label3.Location = new System.Drawing.Point(4, 88);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(41, 12);
             this.label3.TabIndex = 2;
@@ -107,7 +123,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(4, 156);
+            this.label4.Location = new System.Drawing.Point(4, 121);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(77, 12);
             this.label4.TabIndex = 3;
@@ -119,9 +135,9 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.accountComboBox.FormattingEnabled = true;
-            this.accountComboBox.Location = new System.Drawing.Point(159, 4);
+            this.accountComboBox.Location = new System.Drawing.Point(88, 4);
             this.accountComboBox.Name = "accountComboBox";
-            this.accountComboBox.Size = new System.Drawing.Size(149, 20);
+            this.accountComboBox.Size = new System.Drawing.Size(139, 20);
             this.accountComboBox.TabIndex = 4;
             // 
             // nameLabel
@@ -130,9 +146,9 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.nameLabel.AutoSize = true;
-            this.nameLabel.Location = new System.Drawing.Point(159, 48);
+            this.nameLabel.Location = new System.Drawing.Point(88, 56);
             this.nameLabel.Name = "nameLabel";
-            this.nameLabel.Size = new System.Drawing.Size(149, 51);
+            this.nameLabel.Size = new System.Drawing.Size(139, 31);
             this.nameLabel.TabIndex = 5;
             this.nameLabel.Text = "label";
             this.nameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -143,9 +159,9 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.idLabel.AutoSize = true;
-            this.idLabel.Location = new System.Drawing.Point(159, 100);
+            this.idLabel.Location = new System.Drawing.Point(88, 88);
             this.idLabel.Name = "idLabel";
-            this.idLabel.Size = new System.Drawing.Size(149, 55);
+            this.idLabel.Size = new System.Drawing.Size(139, 32);
             this.idLabel.TabIndex = 6;
             this.idLabel.Text = "label";
             this.idLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -156,12 +172,152 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.serverLabel.AutoSize = true;
-            this.serverLabel.Location = new System.Drawing.Point(159, 156);
+            this.serverLabel.Location = new System.Drawing.Point(88, 121);
             this.serverLabel.Name = "serverLabel";
-            this.serverLabel.Size = new System.Drawing.Size(149, 47);
+            this.serverLabel.Size = new System.Drawing.Size(139, 28);
             this.serverLabel.TabIndex = 7;
             this.serverLabel.Text = "label";
             this.serverLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(4, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 12);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "label5";
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.passwordTextBox.Location = new System.Drawing.Point(88, 29);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(139, 21);
+            this.passwordTextBox.TabIndex = 9;
+            // 
+            // balanceCheckButton
+            // 
+            this.balanceCheckButton.Location = new System.Drawing.Point(317, 25);
+            this.balanceCheckButton.Name = "balanceCheckButton";
+            this.balanceCheckButton.Size = new System.Drawing.Size(109, 68);
+            this.balanceCheckButton.TabIndex = 2;
+            this.balanceCheckButton.Text = "계좌 잔고 요청";
+            this.balanceCheckButton.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.BackColor = System.Drawing.Color.White;
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.label6, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.totalPurchaseLabel, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label8, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.totalEstimateLabel, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.label10, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.totalProfitLabel, 1, 2);
+            this.tableLayoutPanel2.Controls.Add(this.label12, 0, 3);
+            this.tableLayoutPanel2.Controls.Add(this.totalProfitRateLabel, 1, 3);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(30, 231);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 4;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 46.9697F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 23F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 27F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(231, 100);
+            this.tableLayoutPanel2.TabIndex = 3;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 12);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "label6";
+            // 
+            // totalPurchaseLabel
+            // 
+            this.totalPurchaseLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.totalPurchaseLabel.AutoSize = true;
+            this.totalPurchaseLabel.Location = new System.Drawing.Point(118, 0);
+            this.totalPurchaseLabel.Name = "totalPurchaseLabel";
+            this.totalPurchaseLabel.Size = new System.Drawing.Size(110, 25);
+            this.totalPurchaseLabel.TabIndex = 1;
+            this.totalPurchaseLabel.Text = "0";
+            this.totalPurchaseLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(3, 25);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(38, 12);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "label8";
+            // 
+            // totalEstimateLabel
+            // 
+            this.totalEstimateLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.totalEstimateLabel.AutoSize = true;
+            this.totalEstimateLabel.Location = new System.Drawing.Point(118, 25);
+            this.totalEstimateLabel.Name = "totalEstimateLabel";
+            this.totalEstimateLabel.Size = new System.Drawing.Size(110, 23);
+            this.totalEstimateLabel.TabIndex = 3;
+            this.totalEstimateLabel.Text = "0";
+            this.totalEstimateLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(3, 48);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(44, 12);
+            this.label10.TabIndex = 4;
+            this.label10.Text = "label10";
+            // 
+            // totalProfitLabel
+            // 
+            this.totalProfitLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.totalProfitLabel.AutoSize = true;
+            this.totalProfitLabel.Location = new System.Drawing.Point(118, 48);
+            this.totalProfitLabel.Name = "totalProfitLabel";
+            this.totalProfitLabel.Size = new System.Drawing.Size(110, 25);
+            this.totalProfitLabel.TabIndex = 5;
+            this.totalProfitLabel.Text = "0";
+            this.totalProfitLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(3, 73);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(44, 12);
+            this.label12.TabIndex = 6;
+            this.label12.Text = "label12";
+            // 
+            // totalProfitRateLabel
+            // 
+            this.totalProfitRateLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.totalProfitRateLabel.AutoSize = true;
+            this.totalProfitRateLabel.Location = new System.Drawing.Point(118, 73);
+            this.totalProfitRateLabel.Name = "totalProfitRateLabel";
+            this.totalProfitRateLabel.Size = new System.Drawing.Size(110, 27);
+            this.totalProfitRateLabel.TabIndex = 7;
+            this.totalProfitRateLabel.Text = "0";
+            this.totalProfitRateLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
@@ -169,6 +325,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Controls.Add(this.balanceCheckButton);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.axKHOpenAPI1);
             this.Name = "Form1";
@@ -176,6 +334,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI1)).EndInit();
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -192,6 +352,18 @@
         private System.Windows.Forms.Label nameLabel;
         private System.Windows.Forms.Label idLabel;
         private System.Windows.Forms.Label serverLabel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Button balanceCheckButton;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label totalPurchaseLabel;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label totalEstimateLabel;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label totalProfitLabel;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label totalProfitRateLabel;
     }
 }
 
--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -31,11 +31,51 @@ namespace WindowsFormsApp1
             }
         }
 
+        public void encryptPassword(object sender, EventArgs e)
+        {
+            if(sender.Equals(passwordTextBox))
+            {
+                passwordTextBox.PasswordChar = '*';
+                passwordTextBox.MaxLength = 4;
+            }
+        }
+
+        public void ButtonClicked(object sender, EventArgs e)
+        {
+            if(sender.Equals(balanceCheckButton))
+            {
+                if(accountComboBox.Text.Length > 0 && passwordTextBox.Text.Length > 0)
+                {
+                    string accountNumber = accountComboBox.Text;
+                    string password = passwordTextBox.Text;
+
+                    axKHOpenAPI1.SetInputValue("계좌번호", accountNumber);
+                    axKHOpenAPI1.SetInputValue("비밀번호", password);
+                    axKHOpenAPI1.SetInputValue("비밀번호 입력 매체 구분", "00");
+                    axKHOpenAPI1.SetInputValue("조회구분", "1");
+
+                    axKHOpenAPI1.CommRqData("계좌 평가 잔고 내역 요청", "opw00018", 0, "8100");
+                }
+            }
+        }
+
+        public void onReceiveTrData(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveTrDataEvent e)
+        {
+            Console.WriteLine(e.sScrNo);
+            Console.WriteLine(e.sRQName);
+            Console.WriteLine(e.sTrCode);
+            Console.WriteLine(e.sRecordName);
+            Console.WriteLine(e.sPrevNext);
+        }
+
         public Form1()
         {
             InitializeComponent();
             axKHOpenAPI1.CommConnect();
             axKHOpenAPI1.OnEventConnect += onEventConnect;
+            passwordTextBox.TextChanged += encryptPassword;
+            balanceCheckButton.Click += ButtonClicked;
+            axKHOpenAPI1.OnReceiveTrData += onReceiveTrData;
         }
     }
 }
